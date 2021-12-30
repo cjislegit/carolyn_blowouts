97e6b3d969e37c05dc31b68c0ad9bb357791de15
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-12-29 20:01:00 -0800

    Added the content in top div and styled button

--- src/sideBar.js
+++ src/sideBar.js
@@ -3,7 +3,15 @@ import styles from './sideBar.module.css';
 const SideBar = () => {
   return (
     <div className={styles.sideBar}>
-      <div className={styles.top}></div>
+      <div className={styles.top}>
+        <h1>Haircuts and Stuff</h1>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu elementum,
+          velit suspendisse sed faucibus quis viverra. Ac risus dictum pulvinar
+          pulvinar ut.
+        </div>
+        <input type='button' value='Book Now' />
+      </div>
       <div className={styles.bottom}></div>
     </div>
   );
