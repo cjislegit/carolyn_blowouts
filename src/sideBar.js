9c60012c4f37bb7424bc13c45f58210d953113d5
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-12-29 19:44:02 -0800

    Place bottom and top divs

--- src/sideBar.js
+++ src/sideBar.js
@@ -1,7 +1,12 @@
 import styles from './sideBar.module.css';
 
 const SideBar = () => {
-  return <div className={styles.sideBar}></div>;
+  return (
+    <div className={styles.sideBar}>
+      <div className={styles.top}></div>
+      <div className={styles.bottom}></div>
+    </div>
+  );
 };
 
 export default SideBar;
