7f058d48a04d3fe4a8242649c5d12736929b2ca8
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-12-30 20:58:25 -0800

    Started adding in photos to gallery

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,36 @@
 import styles from './gallery.module.css';
+import img1 from './assets/IMG_4685.jpeg';
+import img2 from './assets/IMG_4687.jpeg';
+import img3 from './assets/IMG_4663.jpeg';
+import img4 from './assets/IMG_4664.jpeg';
+import img5 from './assets/IMG_4665.jpeg';
+import img6 from './assets/IMG_4666.jpeg';
+import img7 from './assets/IMG_4667.jpeg';
+import img8 from './assets/IMG_4668.jpeg';
+import img9 from './assets/IMG_4669.jpeg';
+import img10 from './assets/IMG_4670.jpeg';
 
 const Gallery = () => {
-  return <div className={styles.gallery}></div>;
+  return (
+    <div className={styles.gallery}>
+      <div className={styles.img}>
+        <img src={img1} alt='' />
+      </div>
+      <div className={styles.img}>
+        <img src={img2} alt='' />
+      </div>
+      <div className={styles.img}>
+        <img src={img3} alt='' />
+      </div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+      <div className={styles.img}></div>
+    </div>
+  );
 };
 
 export default Gallery;
