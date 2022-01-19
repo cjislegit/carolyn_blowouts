a72794592923eedd7ec32333bd5f6d5f2ffdfabb
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-01-18 19:29:33 -0800

    Added more alt text for imgs

--- src/gallery.js
+++ src/gallery.js
@@ -13,16 +13,19 @@ import img10 from './assets/IMG_4670.jpeg';
 const Gallery = () => {
   return (
     <div className={styles.gallery}>
-      <img src={img1} alt='' />
+      <img src={img1} alt='Low ponytail updo' />
       <img src={img2} alt='' />
-      <img src={img3} alt='' />
-      <img src={img4} alt='Silk Press on 3a hair with curls' />
-      <img src={img5} alt='' />
-      <img src={img6} alt='' />
-      <img src={img7} alt='' />
-      <img src={img8} alt='' />
+      <img src={img3} alt='Silk Press on 3a hair with curls' />
+      <img
+        src={img4}
+        alt='Fringe trim on platnum blonde hair with sleek blowout'
+      />
+      <img src={img5} alt='Beachy blonde hairstyle' />
+      <img src={img6} alt='Blonde highlights on a voluminous blowout' />
+      <img src={img7} alt='Braiden half updo' />
+      <img src={img8} alt='Beachy waves with side braid' />
       <img src={img9} alt='' />
-      <img src={img10} alt='' />
+      <img src={img10} alt='Soft casual blowout' />
     </div>
   );
 };
